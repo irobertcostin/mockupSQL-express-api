5f5ae701256bfb1c525b0abf964d61d86122afd0
irobertcostin <irobertcostin@gmail.com>
2023-04-28 21:23:24 +0300

    task completed

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express,{ json, request, response } from "express";
+
+// express server or node app
+const app = express();
+
+// which returns json , easy to consume
+app.use(express.json());
+
+
+// middle-ware
+// cors allows to send requests from other origins to the back-end
+// npm install cors 
+// all CORS (routes) enabled
+app.use(cors());
